d381c79ce600619669e62f0ae953251922206b1a
Febrio Dwi Setiawan <febriodwi14@gmail.com>
2024-05-21 20:48:18 +0700

    menambahkan file index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "hello world";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>remote git</title>
+</head>
+
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias illo corrupti voluptatem quidem molestias error ea iusto blanditiis distinctio, veniam sed deserunt explicabo modi praesentium accusamus! Quaerat provident harum vero!</p>
+</body>
+
+</html>
